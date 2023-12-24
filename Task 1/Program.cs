6a69e1951f8b014c88580f0e3a64497074ca8140
Murad <baghmr24@gmail.com>
2023-12-24 18:53:02 +0400

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+int GetStringToInteger(int number)
+{
+    Console.WriteLine($"Введите {number} число:");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+void ValuesBetweenTwoNumbers(int number1, int number2)
+{
+    if (number1>number2)
+    {
+        Console.WriteLine(number1-1);
+        ValuesBetweenTwoNumbers(number1-1,number2);
+    }
+    else if (number2>number1)
+    {
+        Console.WriteLine(number2-1);
+        ValuesBetweenTwoNumbers(number2-1,number1);
+    }
+}
+int m = Convert.ToInt32(GetStringToInteger(1));
+int n = Convert.ToInt32(GetStringToInteger(2));
+Console.WriteLine();
+ValuesBetweenTwoNumbers(m,n);
